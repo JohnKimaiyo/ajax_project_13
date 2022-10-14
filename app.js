257d58430f82f79dab0924993b580b9d4a0903b4
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-14 10:02:43 +0300

    setting up the post http request

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { brainhttp } from './brainhttp';
+const serverURL = `http://127.0.0.1:3001/api/employees`
 // getbutton
 let getButton = document.querySelector('#get-btn');
 getButton.addEventListener('click', function () {
@@ -5,5 +7,36 @@ getButton.addEventListener('click', function () {
 });
 
 let fetchEmployees = () => {
+    let http = new brainhttp();
+    let url = `${serverURL}/employees`;
+    http.get(url, (err, employees) => {
+        if (err) throw err;
+        let tableRows = '';
+        for (let employee of employees) {
 
+
+            tableRows += `<tr>
+                <td>${employee.id}</td>
+                <td>${employee.first_name}</td>
+                <td>${employee.last_name}</td>
+                <td>${employee.email}</td>
+                <td>${employee.gender}</td>
+                <td>${employee.ip_address}</td>
+                </tr>
+                `
+        }
+        document.querySelector('#table-body').innerHTML = tableRows
+    });
+    console.log(users);
 }
+
+
+
+
+
+
+
+
+
+
+
